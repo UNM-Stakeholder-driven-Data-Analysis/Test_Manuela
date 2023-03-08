f8c37b4751fafd6b3bee4338989989cb0b34d07b
“Manuela” <manuelalg28@gmail.com>
2023-03-08 09:43:50 -0700

    River Eyes data is dry RM per year

--- Test_scripts/00_data_USGS-Summer.R
+++ Test_scripts/00_data_USGS-Summer.R
@@ -1,15 +1,13 @@
 #### READ ME####
 #The purpose of this script is to gather the data from both USGS gage system 
-#and the River Eyes dataset and explore both of them by.
+# FOR THE SUMMER ONLY - APRIL 1st TO OCTOBER 31st FOR EVERY YEAR and explore it
 # - describing dataset size (# variables & # observations)
 # - describing data types
 # - checking data distributions
 # - checking for spatial autocorrelation
 # - checking for temporal autocorrelation
-# - checking for correlation between variables
 
-install.packages("tidyverse")
-#### packages ####
+#### libraries ####
 library(dataRetrieval) #USGS data pagacke
 library(tidyverse)
 library(lubridate)
@@ -43,91 +41,13 @@ vignette("dataRetrieval", package = "dataRetrieval")
 # 8332010	RIO GRANDE FLOODWAY NEAR BERNARDO, NM
 # 8354900	RIO GRANDE FLOODWAY AT SAN ACACIA, NM
 
-#### retrieving data USGS ####
-
-# retrieve data for one site
-otowi <- readNWISdata(siteNumbers = "08313000",
-                      parameterCd = "00060",
-                      startDate = "2002-04-01",
-                      endDate = "2022-04-01",
-                      service="dv")
-
-#retrieve data for all dates on all 8 gages
-siteNumber <- c("08313000", "08313150", "08317400", "08319000", "08329918", "08329928", 
-                "08330000", "08330830", "08330875", "08331160", "08331510", "08332010", 
-                "08354900")
-#this code retrieves the discharge data
-pCode <- "00060"
-#start and end date from 2002 to 2022
-sdate <- "2002-04-01"
-edate <- "2022-10-31"
-
-#get all the data from 2002 to 2022
-fulldischarge <- readNWISdv(siteNumbers = siteNumber,
-                            parameterCd = pCode,
-                            startDate = sdate,
-                            endDate = edate,
-                            )
-
-# add year and day of year for plotting
-fulldischarge$year = lubridate::year(fulldischarge$Date)
-fulldischarge$day = lubridate::yday(fulldischarge$Date)
-
-#are there any missing values?
-sum(is.na(fulldischarge$X_00060_00003)) #no
-
-#separate data set by sites for plotting for Otowi
-dis_otowi <- 
-  fulldischarge %>% 
-  filter(site_no=="08319000") %>%
-  arrange(Date)
-
-# add year and day of year for plotting Just did that for whole dataset
-dis_otowi$year = lubridate::year(dis_otowi$Date)
-dis_otowi$day = lubridate::yday(dis_otowi$Date)
-
-# plot discharge by year 
-ggplot(data=dis_otowi, aes(x=day, y=X_00060_00003))+
-  geom_point() + geom_path()+
-  facet_wrap(~year, scales="free_y")+
-  theme(legend.title = element_blank()) +
-  theme_bw()
-
-# can also look at single years in detail
-ggplot(data=dis_otowi, aes(x=Date, y=X_00060_00003))+
-  geom_point() + geom_path()+
-  xlim(c(as.Date("2019-04-01"), as.Date("2019-10-31")))+
-  theme(legend.title = element_blank()) +
-  theme_bw()
-
-#separate data set by sites for plotting for San Acacia
-dis_sanacacia <- 
-  fulldischarge %>% 
-  filter(site_no=="08354900") %>%
-  arrange(Date)
-
-# add year and day of year for plotting Just did that for whole dataset
-dis_sanacacia$year = lubridate::year(dis_sanacacia$Date)
-dis_sanacacia$day = lubridate::yday(dis_sanacacia$Date)
-
-# plot discharge by year 
-ggplot(data=dis_sanacacia, aes(x=day, y=X_00060_00003))+
-  geom_point() + geom_path()+
-  facet_wrap(~year, scales="free_y")+
-  theme(legend.title = element_blank()) +
-  theme_bw()
-
-# can also look at single years in detail
-ggplot(data=dis_sanacacia, aes(x=Date, y=X_00060_00003))+
-  geom_point() + geom_path()+
-  xlim(c(as.Date("2019-04-01"), as.Date("2019-10-31")))+
-  theme(legend.title = element_blank()) +
-  theme_bw()
-
 #### retrieving data USGS for the summer only####
+
 #use paste function and for loop to get all the dates from only the summer
+#from 2003 to 208, because of the River Eyes Data 
+
 #start date
-start.date1 = c(2002:2022)
+start.date1 = c(2003:2018)
 start.date <- vector(mode="character", length=length(start.date1))
 for (i in 1:length(start.date1)){
   start.date[i] = paste(start.date1[i], "04","01", sep="-")
@@ -137,55 +57,78 @@ start.date = as.Date(start.date)
 #do I need it as a date?
 
 #end date
-end.date1 = c(2002:2022)
+end.date1 = c(2003:2018)
 end.date <- vector("character", length(end.date1))
 for (i in seq_along(end.date1)){
   end.date[i] = paste(end.date1[i], "10","31", sep="-")
 }
 end.date
+end.date = as.Date(end.date)
 
-#get all the data from 2002 to 2022
-discharge <- readNWISdv(siteNumbers = siteNumber,
-                        parameterCd = pCode,
-                        startDate = start.date,
-                        endDate = end.date
+# Initialize an empty data frame to hold the results
+discharge <- data.frame()
+
+# Loop through each date range and retrieve the corresponding data
+for (i in seq_along(start.date)) {
+  # Retrieve data for the current date range
+  temp <- readNWISdv(siteNumbers = siteNumber,
+                     parameterCd = pCode,
+                     startDate = start.date[i],
+                     endDate = end.date[i])
+  
+  # Append the data to the results data frame
+  discharge <- rbind(discharge, temp)
+}
+
+# View the resulting data frame
+discharge
 )
-
-# data from Rio Grande? fix
-#data_nm <- readNWISdata(huc="13020211", parameterCd="00060", service="uv")
-
 
 #### check distributions####
 #Otowi
-temp = fulldischarge[fulldischarge$site_no == "08313000",]
+temp = discharge[discharge$site_no == "08313000",]
 qqPlot(temp$X_00060_00003); shapiro.test(temp$X_00060_00003[0:5000]) # not normal
 #White Rock
-temp = fulldischarge[fulldischarge$site_no == "08313150",]
+temp = discharge[discharge$site_no == "08313150",]
 qqPlot(temp$X_00060_00003); shapiro.test(temp$X_00060_00003[0:5000]) # not normal
 #Cochiti
-temp = fulldischarge[fulldischarge$site_no == "08317400",]
+temp = discharge[discharge$site_no == "08317400",]
+qqPlot(temp$X_00060_00003); shapiro.test(temp$X_00060_00003[0:5000]) # not normal
+#Albuquerque
+temp = discharge[discharge$site_no == "08330000",]
+qqPlot(temp$X_00060_00003); shapiro.test(temp$X_00060_00003[0:5000]) # not normal
+#Bosque Farms
+temp = discharge[discharge$site_no == "08331160",]
+qqPlot(temp$X_00060_00003); shapiro.test(temp$X_00060_00003[0:5000]) # not normal
+#Isleta
+temp = discharge[discharge$site_no == "08330875",]
 qqPlot(temp$X_00060_00003); shapiro.test(temp$X_00060_00003[0:5000]) # not normal
 #San Acacia
-temp = fulldischarge[fulldischarge$site_no == "08354900",]
+temp = discharge[discharge$site_no == "08354900",]
 qqPlot(temp$X_00060_00003); shapiro.test(temp$X_00060_00003[0:5000]) # not normal
 
 #Otowi
-temp = fulldischarge[fulldischarge$site_no == "08313000",]
+temp = discharge[discharge$site_no == "08313000",]
 summary(temp$X_00060_00003)
 hist(temp$X_00060_00003)
 plot(density(temp$X_00060_00003))
 #White Rock
-temp = fulldischarge[fulldischarge$site_no == "08313150",]
+temp = discharge[discharge$site_no == "08313150",]
 summary(temp$X_00060_00003)
 hist(temp$X_00060_00003)
 plot(density(temp$X_00060_00003))
 #Cochiti
-temp = fulldischarge[fulldischarge$site_no == "08317400",]
+temp = discharge[discharge$site_no == "08317400",]
+summary(temp$X_00060_00003)
+hist(temp$X_00060_00003)
+plot(density(temp$X_00060_00003))
+#Albuquerque
+temp = discharge[discharge$site_no == "08330000",]
 summary(temp$X_00060_00003)
 hist(temp$X_00060_00003)
 plot(density(temp$X_00060_00003))
 #San Acacia
-temp = fulldischarge[fulldischarge$site_no == "08354900",]
+temp = discharge[discharge$site_no == "08354900",]
 summary(temp$X_00060_00003)
 hist(temp$X_00060_00003)
 plot(density(temp$X_00060_00003))
@@ -196,31 +139,34 @@ plot(density(temp$X_00060_00003))
 # what happens if I log-transform it?
 
 #otowi
-temp = fulldischarge[fulldischarge$site_no == "08313000",]
+temp = discharge[discharge$site_no == "08313000",]
 qqPlot(log10(temp$X_00060_00003)); shapiro.test(log10(temp$X_00060_00003[0:5000]))
 #white rock
-temp = fulldischarge[fulldischarge$site_no == "08313150",]
+temp = discharge[discharge$site_no == "08313150",]
 qqPlot(log10(temp$X_00060_00003)); shapiro.test(log10(temp$X_00060_00003))
 #cochiti
-temp = fulldischarge[fulldischarge$site_no == "08317400",]
+temp = discharge[discharge$site_no == "08317400",]
 qqPlot(log10(temp$X_00060_00003)); shapiro.test(log10(temp$X_00060_00003[0:5000]))
-#san acacia
-temp = fulldischarge[fulldischarge$site_no == "08354900",]
+#Albuquerque
+temp = discharge[discharge$site_no == "08330000",]
 qqPlot(log10(temp$X_00060_00003[0:5000])); shapiro.test(log10(temp$X_00060_00003[0:5000]))
-#san acacia is not working!!!!!!!!!!!!!!!
+#isleta
+temp = discharge[discharge$site_no == "08330875",]
+qqPlot(log10(temp$X_00060_00003[0:5000])); shapiro.test(log10(temp$X_00060_00003[0:5000]))
 
 range(temp$X_00060_00003)
 is.infinite(temp$X_00060_00003)
 
 # a log10 transformation did the trick! That tells me that it is lognormal. I will note in my report that a log10 transformation is a possible option if my models don't meet assumptions.
 # Also note the stair-steps in the data at lower values. This could result from detection limits where the low value was replaced with a standard value. It shouldn't be a huge problem, but it is worth noting as a thing to investigate if the analyses don't turn out well. 
-
-
+#for now, it doesn't matter if I can't "name the distribution of my data". It will
+# be important down the road when I'm dealing with the residuals in my model, but 
+# for now it's ok.
 
 #### temporal autocorrelation####
 #### Otowi
 ### subset data to be one site and one parameter
-temp = fulldischarge[fulldischarge$site_no == alist("08313000") & fulldischarge$X_00060_00003,]
+temp = discharge[discharge$site_no == alist("08313000") & discharge$X_00060_00003,]
 ### make this a time series object
 ## first, make doubly sure that the data is arranged by time before converting to ts object!
 temp = temp %>% arrange(Date) 
@@ -236,7 +182,7 @@ temp_ts =
 # - the frequency, which is the number of observations per unit of time. Lots of ways to specify this. For monthly data, you can put in 12 and it will assume that's 12 obs in a year. Google for help for other frequencies.
 # - the start, which specifies when the first obs occurred. Lots of ways to specify this. For monthly data, you can put in c(year, month) and it will know what you mean. 
 head (temp_ts)
-temp_ts = ts(temp_ts$X_00060_00003, frequency=365, start=c(2002, 04, 01)) 
+temp_ts = ts(temp_ts$X_00060_00003, frequency=365, start=c(2003, 04, 01)) 
 # check that you specified the ts correctly
 print(temp_ts, calendar = T) 
 
@@ -256,7 +202,7 @@ forecast::Pacf(temp_ts, na.action = na.interp)
 
 #### San Acacia
 ### subset data to be one site and one parameter
-temp = fulldischarge[fulldischarge$site_no == alist("08354900") & fulldischarge$X_00060_00003,]
+temp = discharge[discharge$site_no == alist("08354900") & discharge$X_00060_00003,]
 ### make this a time series object
 ## first, make doubly sure that the data is arranged by time before converting to ts object!
 temp = temp %>% arrange(Date) 
@@ -272,7 +218,7 @@ temp_ts =
 # - the frequency, which is the number of observations per unit of time. Lots of ways to specify this. For monthly data, you can put in 12 and it will assume that's 12 obs in a year. Google for help for other frequencies.
 # - the start, which specifies when the first obs occurred. Lots of ways to specify this. For monthly data, you can put in c(year, month) and it will know what you mean. 
 head (temp_ts)
-temp_ts = ts(temp_ts$X_00060_00003, frequency=365, start=c(2019, 04, 01)) 
+temp_ts = ts(temp_ts$X_00060_00003, frequency=365, start=c(2018, 04, 01)) 
 # check that you specified the ts correctly
 print(temp_ts, calendar = T) 
 
@@ -291,19 +237,19 @@ forecast::Pacf(temp_ts, na.action = na.interp)
 
 #### spatial autocorrelation ####
 # how many sites are there?
-length(unique(fulldischarge$site_no))
+length(unique(discharge$site_no))
 # 12
-#obtain information available for a particular USGS site (or sites)
+#obtain information available (i.e. lat long) for a particular USGS site (or sites)
 siteInfo <- readNWISsite(c("08313000", "08313150", "08317400", "08319000", "08329918", "08329928", 
-                 "08330000", "08330830", "08330875", "08331160", "08331510", "08332010", 
-                 "08354900"))
+                           "08330000", "08330830", "08330875", "08331160", "08331510", "08332010", 
+                           "08354900"))
 #merge the coordinates to my full data frame
-fulldischarge <- merge(fulldischarge, siteInfo[,c("site_no", "lat_va")], by = "site_no", all.x = TRUE)
-fulldischarge <- merge(fulldischarge, siteInfo[,c("site_no", "long_va")], by = "site_no", all.x = TRUE)
+discharge <- merge(discharge, siteInfo[,c("site_no", "lat_va")], by = "site_no", all.x = TRUE)
+discharge <- merge(discharge, siteInfo[,c("site_no", "long_va")], by = "site_no", all.x = TRUE)
 
 ### winter day 2019
-dat_winter = fulldischarge[fulldischarge$Date >= as.Date("2019-02-01") &
-                           fulldischarge$Date < as.Date("2019-02-02"),]
+dat_winter = discharge[discharge$Date >= as.Date("2018-02-01") &
+                             discharge$Date < as.Date("2018-02-02"),]
 temp = dat_winter %>%  filter(X_00060_00003 %in% dat_winter)
 
 ## Moran.I winter 2019
@@ -315,8 +261,8 @@ diag(dists.inv) = 0
 Moran.I(dat_winter$X_00060_00003, dists.inv)
 
 #### spring day 2019
-dat_spring = fulldischarge[fulldischarge$Date >= as.Date("2019-04-01") &
-                             fulldischarge$Date < as.Date("2019-04-02"),]
+dat_spring = discharge[discharge$Date >= as.Date("2018-04-01") &
+                             discharge$Date < as.Date("2018-04-02"),]
 
 ## Moran.I spring 
 # generate an inverse distance matrix 
@@ -327,8 +273,8 @@ diag(dists.inv) = 0
 Moran.I(dat_spring$X_00060_00003, dists.inv)
 
 #### fall day 2019
-dat_fall = fulldischarge[fulldischarge$Date >= as.Date("2019-10-01") &
-                             fulldischarge$Date < as.Date("2019-10-02"),]
+dat_fall = discharge[discharge$Date >= as.Date("2019-10-01") &
+                           discharge$Date < as.Date("2019-10-02"),]
 
 ## Moran.I fall
 # generate an inverse distance matrix 
@@ -339,8 +285,8 @@ diag(dists.inv) = 0
 Moran.I(dat_fall$X_00060_00003, dists.inv)
 
 #### Monsoon day 2019
-dat_monsoon = fulldischarge[fulldischarge$Date >= as.Date("2019-06-01") &
-                           fulldischarge$Date < as.Date("2019-06-02"),]
+dat_monsoon = discharge[discharge$Date >= as.Date("2019-06-01") &
+                              discharge$Date < as.Date("2019-06-02"),]
 
 ## Moran.I Monsoon
 # generate an inverse distance matrix 
