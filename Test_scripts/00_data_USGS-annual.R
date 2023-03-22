851ecabefc97c211bfb0bfadc561ced49313afd8
“Manuela” <manuelalg28@gmail.com>
2023-03-22 12:57:51 -0600

    tweaking model structure

--- Test_scripts/00_data_USGS-annual.R
+++ Test_scripts/00_data_USGS-annual.R
@@ -124,6 +124,58 @@ bernardo <- readNWISdata(siteNumbers = "08332010",
                           endDate = "2018-10-31",
                           service="dv")
 
+####Linear models####
+
+# Filter data for a specific site
+site_no <- "08313000" #Otowi gauge
+RM <- "248" #RM for Otowi 257???
+site_year_data <- discharge_sum %>%
+  filter(site_no == site_no, RM == RM)
+
+# create the linear model
+m1 <- lm(Sum_days_rm_dry ~ discharge_sum, data = site_year_data)
+summary(m1)
+# check assumptions
+plot(m1)
+
+# Filter data for a specific site and year
+site_no <- "08313150"
+RM <- "254"
+site_year_data <- discharge_sum %>%
+  filter(site_no == site_no, RM == RM)
+
+# create the linear model
+m1 <- lm(Sum_days_rm_dry ~ discharge_sum, data = site_year_data)
+summary(m1)
+# check assumptions
+plot(m1)
+
+####GLM####
+# Filter data for a specific site and year
+site_no <- "08313150"
+RM <- "254"
+site_year_data <- merged_data %>%
+  filter(site_no == site_no, RM == RM)
+# build model
+glm_full <- glm(Sum_days_rm_dry ~ discharge_sum, 
+                family = "poisson", 
+                data = site_year_data)
+
+#satisfies the assumptions
+simulateResiduals(glm_full, plot = T)
+
+#run anova
+car::Anova(glm_full, type = "III")
+
+# Filter data for a specific site and year
+site <- "08313000"
+rivermile <- "54"
+
+site_year_data <- merged_data %>%
+  filter(site_no == site & RM == rivermile)
+
+tmod_lme4_L <- glmer(Sum_days_rm_dry ~ discharge_sum  + (1|year),
+                     family = poisson(link = "log"), data= site_year_data)
 
 
 
