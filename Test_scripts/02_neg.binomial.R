f4cda3a6790074b091a57e9d21b1442072980f52
“Manuela” <manuelalg28@gmail.com>
2023-04-12 21:05:15 -0600

    testing more models
    zero-inflated poisson, negative binomial, poisson for now

---
+++ Test_scripts/02_neg.binomial.R
@@ -0,0 +1,95 @@
+####READ ME####
+#The purpose of this script is to run a Poisson model to obtain error values
+#then make a table with the gauge, RM, distance between RM and gauge and error of each GLM for that pair 
+
+####Libraries ####
+library(tidyverse)
+library(DHARMa)
+library(lme4) # for creating mixed models
+library(emmeans) # for emmeans, emtrends, all the post hoc tests and plotting
+library(data.table)
+library(reshape2)
+
+####load data frames list and distance matrix####
+#model data frame list 
+alldf <- readRDS("Data/df_list.RData")
+####load data frames list and distance matrix####
+#model data frame list 
+alldf <- readRDS("Data/df_list.RData")
+#distance matrix
+dist_matri <- read.table("Data/distance_matrix.csv", sep = ",", header = TRUE)
+colnames(dist_matri)[1] <- "gauge"  
+#remove X from front of column names
+colnames(dist_matri) <- gsub("X", "", colnames(dist_matri))
+dist_matri$gauge <- paste0("0", dist_matri$gauge)
+#### Negative Binomial ####
+fit_nb <- lapply(alldf, function(df) {
+  glm_all <- glm.nb(Sum_days_rm_dry ~ discharge_sum, data = df)
+})
+
+#save models
+saveRDS(fit_nb, file="nb.models.RData")
+
+#extract deviance#
+deviance <- lapply(fit_nb,function(df) { 
+  summary(df)[["deviance"]]
+})
+
+
+####testing 10 random models####
+#simulate and plot residuals for one model
+#KS the higher the better, closer to 1?
+#deviation should not be significant
+#Zero inflation and dispersion if p-value small (less than ex 0.05) 
+#we reject the null hypothesis and residuals are zero inflated or overdispersed 
+#1
+simulationOutput <- simulateResiduals(fittedModel = fit_nb[["08330000_100"]])
+plot(simulationOutput, main = "Simulated Residuals 1")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput) #Overdispersion describes the observation that variation is higher than would be expected.
+#2
+simulationOutput <- simulateResiduals(fittedModel = fit_nb[["08317400_133"]])
+plot(simulationOutput, main = "Simulated Residuals 2")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#3
+simulationOutput <- simulateResiduals(fittedModel = fit_nb[["08331160_91"]])
+plot(simulationOutput, main = "Simulated Residuals 3")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#4
+simulationOutput <- simulateResiduals(fittedModel = fit_nb[["08330875_80"]])
+plot(simulationOutput,  main = "Simulated Residuals 4")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#5
+simulationOutput <- simulateResiduals(fittedModel = fit_nb[["08317400_103"]])
+plot(simulationOutput, main = "Simulated Residuals 5")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#6
+simulationOutput <- simulateResiduals(fittedModel = fit_nb[["08329928_57"]])
+plot(simulationOutput, main = "Simulated Residuals 6")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#7
+simulationOutput <- simulateResiduals(fittedModel = fit_nb[["08313000_70"]])
+plot(simulationOutput, main = "Simulated Residuals 7")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#8
+simulationOutput <- simulateResiduals(fittedModel = fit_nb[["08313000_164"]])
+plot(simulationOutput, main = "Simulated Residuals 8")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#9
+simulationOutput <- simulateResiduals(fittedModel = fit_nb[["08331160_66"]])
+plot(simulationOutput, main = "Simulated Residuals 9")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#10
+simulationOutput <- simulateResiduals(fittedModel = fit_nb[["08319000_123"]])
+plot(simulationOutput, main = "Simulated Residuals 10")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+
---
+++ Test_scripts/02_zero.inf.poisson.R
@@ -0,0 +1,82 @@
+####READ ME####
+#The purpose of this script is to run a zero-inflated poisson model to obtain error values
+#then make a table with the gauge, RM, distance between RM and gauge and error of each GLM for that pair 
+#then, I will test the negative binomial regression model using DHARMa residual simulations.
+
+####Libraries ####
+library(tidyverse)
+library(DHARMa) #simulations
+library(lme4) # for creating mixed models
+library(emmeans) # for emmeans, emtrends, all the post hoc tests and plotting
+library(pscl) #zero inflated
+
+install.packages("pscl")
+####load data frames list and distance matrix####
+#model data frame list 
+alldf <- readRDS("Data/df_list.RData")
+#### Zero-inflated Poisson ####
+fit_zip <- lapply(alldf, function(df) {
+  glm_all <- zeroinfl(Sum_days_rm_dry ~ discharge_sum | 1, 
+                      data = df,)
+})
+
+?zeroinfl
+#save models
+saveRDS(fit_glm, file="zip.models.RData")
+
+####testing 10 random models####
+#simulate and plot residuals for one model
+#KS the higher the better, closer to 1?
+#deviation should not be significant
+#Zero inflation and dispersion if p-value small (less than ex 0.05) 
+#we reject the null hypothesis and residuals are zero inflated or overdispersed 
+#1
+simulationOutput <- simulateResiduals(fittedModel = fit_zip[["08330000_100"]])
+plot(simulationOutput, main = "Simulated Residuals 1")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput) #Overdispersion describes the observation that variation is higher than would be expected.
+#2
+simulationOutput <- simulateResiduals(fittedModel = fit_zip[["08317400_133"]])
+plot(simulationOutput, main = "Simulated Residuals 2")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#3
+simulationOutput <- simulateResiduals(fittedModel = fit_zip[["08331160_91"]])
+plot(simulationOutput, main = "Simulated Residuals 3")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#4
+simulationOutput <- simulateResiduals(fittedModel = fit_zip[["08330875_80"]])
+plot(simulationOutput,  main = "Simulated Residuals 4")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#5
+simulationOutput <- simulateResiduals(fittedModel = fit_zip[["08317400_103"]])
+plot(simulationOutput, main = "Simulated Residuals 5")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#6
+simulationOutput <- simulateResiduals(fittedModel = fit_zip[["08329928_57"]])
+plot(simulationOutput, main = "Simulated Residuals 6")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#7
+simulationOutput <- simulateResiduals(fittedModel = fit_zip[["08313000_70"]])
+plot(simulationOutput, main = "Simulated Residuals 7")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#8
+simulationOutput <- simulateResiduals(fittedModel = fit_zip[["08313000_164"]])
+plot(simulationOutput, main = "Simulated Residuals 8")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#9
+simulationOutput <- simulateResiduals(fittedModel = fit_zip[["08331160_66"]])
+plot(simulationOutput, main = "Simulated Residuals 9")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
+#10
+simulationOutput <- simulateResiduals(fittedModel = fit_zip[["08319000_123"]])
+plot(simulationOutput, main = "Simulated Residuals 10")
+testZeroInflation(simulationOutput)
+testDispersion(simulationOutput)
